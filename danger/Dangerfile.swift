abc2dc1d423041edc2eefc981671790210eb7069
Andreas <andreas.kompanez@beekeeper.io>
2020-06-03 23:50:36 +0200

    Update Dangerfile.swift

--- danger/Dangerfile.swift
+++ danger/Dangerfile.swift
@@ -3,7 +3,4 @@ import Foundation
 
 let danger = Danger()
 
-// SwiftLint.lint(.modifiedAndCreatedFiles(directory: "Sources"), inline: true)
 SwiftLint.lint(inline: true, configFile: ".swiftlint.yml", strict: true)
-
-
